5dfa0cbf213c32f62805575cb7609f33f2598a2f
Liam Idrovo <liamidrovo@gmail.com>
2023-06-21 10:06:00 -0700

    checks user Spotify premium status and removed light/dark theme option

---
+++ src/utils/isUserPremium.ts
@@ -0,0 +1,20 @@
+import { SpotifyToken } from '~/types';
+
+export const isUserPremium = async (token: SpotifyToken) => {
+  try {
+    const response = await fetch('https://api.spotify.com/v1/me', {
+      headers: {
+        Authorization: `Bearer ${token.accessToken}`,
+      },
+    });
+
+    if (!response.ok) return false;
+    const { product } = await response.json();
+
+    console.log({ product });
+    return product === 'premium';
+  } catch (error) {
+    console.log({ error });
+    return false;
+  }
+};
